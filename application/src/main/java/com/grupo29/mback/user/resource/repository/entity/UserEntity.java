aff69e5b944d2ccbc64578842b66842456db2ecb
Caio Lacerda <caiolacerda88@gmail.com>
2024-03-25 10:59:06 -0300

    Fixing error in user entity table name

--- application/src/main/java/com/grupo29/mback/user/resource/repository/entity/UserEntity.java
+++ application/src/main/java/com/grupo29/mback/user/resource/repository/entity/UserEntity.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
+@Table(name = "`user`")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
